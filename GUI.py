8cf72c07d6cbc2b61ec2ff7f3dc07d0d0b79ec6b
jose <douglasquan18@gmail.com>
2022-01-17 01:56:38 -0500

    commit

--- GUI.py
+++ GUI.py
@@ -1,21 +1,42 @@
 from tkinter import *
+import sys
+import os 
 
 root = Tk()
 root.geometry("1200x600")
 root.title("S&P50")
-root.configure(bg = "#383847")
+root.configure(bg = "#2D2D32")
 
-title = Label(root, text = "S&P 50", font = ("Lora",50))
+title = Label(root, text = "S&P 10", fg = "#D5D5D6", bg = '#2D2D32', font = ("Times New Roman",70, "bold"))
 title.pack()
 
-crypto = Entry(root, width = 50, borderwidth = 5, )
+
+def getCrypto():
+    info = Label(root, text = crypto.get())
+    info.pack()
+
+crypto = Entry(root, width = 50, borderwidth = 1, bg = '#2D2D32')
 crypto.pack()
+crypto.insert(0, "Enter Crypto")
 
 
 
-    
-randomButton =  Button(root, text = "Next Crypto", padx = 50, pady = 50)
+getInfo_Button = Button(root, text = "Get Info", padx = 100, pady = 70, font = ("Times New Roman",20, "bold"), command = getCrypto)
+getInfo_Button.pack()
+getInfo_Button.place(x = 200, y = 180)
+
+
+
+
+def randomCrypto():
+    os.system("python main.py")
+
+randomButton = Button(root, text = "Random Crypto", bg = "#7C7C7C", padx = 70, pady = 70, font = ("Times New Roman",20, "bold"), command = randomCrypto)
 randomButton.pack()
+randomButton.place(x = 700, y = 180)
+
+
+
 
 
 root.mainloop()
